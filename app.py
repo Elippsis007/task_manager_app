8cc62dd16b66a1b921145531fe9284c2dad3bd8d
Elippsis007 <michaeldrmm@gmail.com>
2021-09-10 12:50:05 +0000

    removed comment out lines from code from tasks.html as it was affecting the output on web browser

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import os
 # Importing the Flask module
-from flask import (
-    Flask, flash, render_template, redirect, 
-    request, session, url_for) 
 # In order to use the environment variables 'env.py', we need to import the 'env' package
+# MongoDB stores its data in a JSON-like format called BSON, In order to find documents from MongoDB later, we need to be able to render the ObjectId
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId # MongoDB stores its data in a JSON-like format called BSON, In order to find documents from MongoDB later, we need to be able to render the ObjectId
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env 
 
@@ -13,30 +14,38 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-
 # Adding configurations to my app
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME") # Grab the database name: app.config["MONGO_DBNAME"]
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI") # To configure the actual connection string, also called the MONGO_URI
-app.secret_key = os.environ.get("SECRET_KEY") # Grab our SECRET_KEY, which is a requirement when using some of the functions from Flask: app.secret_key = os.environ.get("SECRET_KEY")
+# Grab the database name: app.config["MONGO_DBNAME"]
+# To configure the actual connection string, also called the MONGO_URI
+# Grab our SECRET_KEY, which is a requirement when using some of the functions from Flask: app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY") 
 
 
 # I need to setup an instance of PyMongo, and add the app into a **Constructor Method**
 # This is the Flask 'app' object I have defined above, and is the final step to ensure my Flask app is properly communicating with the Mongo database
-mongo = PyMongo(app) 
+mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks") # The routing is a string that, when we attach it to a URL, will redirect to a particular function in our Flask app.
+# The routing is a string that, when we attach it to a URL, will redirect to a particular function in our Flask app.
+@app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find() # This will find all documents from the tasks collection, and assign them to our new 'tasks' variable.
-    return render_template("tasks.html", tasks=tasks) # This tasks template, I want to be able to generate data from my tasks collection on MongoDB, visible to our users
-                                        # The first 'tasks' is what the template will use, and that's equal to the second 'tasks',
+    # This will find all documents from the tasks collection, and assign them to our new 'tasks' variable.
+    tasks = mongo.db.tasks.find() 
+    # This tasks template, I want to be able to generate data from my tasks collection on MongoDB, visible to our users
+    # The first 'tasks' is what the template will use, and that's equal to the second 'tasks'
+    return render_template("tasks.html", tasks=tasks) 
+                                        
 
-
+# The host will be set to the IP, so we need to type os.environ.get("IP") in order to fetch that default value, which was "0.0.0.0"
+# The port will need to be converted to an integer, so we'll type: int(os.environ.get("PORT"))
+# The final parameter will be debug=True, because during development, we want to see the actual errors that may appear, instead of a generic server warning
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), # The host will be set to the IP, so we need to type os.environ.get("IP") in order to fetch that default value, which was "0.0.0.0"
-            port=int(os.environ.get("PORT")), # The port will need to be converted to an integer, so we'll type: int(os.environ.get("PORT"))
-            debug=True) # The final parameter will be debug=True, because during development, we want to see the actual errors that may appear, instead of a generic server warning
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
 
 
 # I need to setup some files that Heroku needs to run the app. First, I need to tell Heroku 
