a689c672ee31273f0ac93050e55d7593982036a7
Elippsis007 <michaeldrmm@gmail.com>
2021-09-14 15:35:45 +0000

    Added Session User Profile

--- app.py
+++ app.py
@@ -53,13 +53,16 @@ def register():
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(
+                request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # Put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for(
+            "profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -73,9 +76,13 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+                        
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -87,6 +94,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
                                         
 
